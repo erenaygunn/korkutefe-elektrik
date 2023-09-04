b251dd233c1f0c0e90c0911b05d1201047750c49
erenaygunn <eaygun178@gmail.com>
2023-09-04 14:52:26 +0300

    hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,6 +6,11 @@ const Hero = () => {
   return (
     <section className='hero'>
         <Navbar></Navbar>
+        <div className="container">
+          <h1>Korkut Efe Elektriğe Hoş geldiniz</h1>
+          <h4>Muğla ve civarındaki bütün elektrik işlerinizde <br /> bize güvenebilirsiniz.</h4>
+          <button className='action'>DEVAM ET</button>
+        </div>
     </section>
   )
 }
