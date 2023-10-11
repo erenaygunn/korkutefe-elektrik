d4aee4fc555d28a49c7b570bd0ad3204ad13ca36
erenaygunn <eaygun178@gmail.com>
2023-10-11 21:49:55 +0300

    iletisim

--- src/components/iletisim/iletisim.jsx
+++ src/components/iletisim/iletisim.jsx
@@ -7,22 +7,22 @@ import email from "../../images/email.png"
 const Iletisim = () => {
   return (
     <section className='iletisim'>
-      <h5>İLETİŞİM</h5>
-      <div className="iletisim-container" data-aos="fade-up">
-        <div className="iletisim-card">
+      <h5 data-aos='fade-down'>İLETİŞİM</h5>
+      <div className="iletisim-container">
+        <div className="iletisim-card" data-aos="fade-up">
           <img src={email} alt="email" />
             <h6>Mail atın</h6>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam quam iste, voluptatum perferendis similique repudiandae culpa architecto excepturi doloremque iure.</p>
+            <p>info@korkutefeelektrik.com</p>
         </div>
-        <div className="iletisim-card">
+        <div className="iletisim-card" data-aos="fade-up">
           <img src={pin} alt="pin" />
           <h6>Adresimiz</h6>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam quam iste, voluptatum perferendis similique repudiandae culpa architecto excepturi doloremque iure.</p>
+          <p>Muslihittin Mahallesi, Mehmet Zekai Özbek Sk. No:51/A, 48000 Menteşe/Muğla</p>
         </div>
-        <div className="iletisim-card">
+        <div className="iletisim-card" data-aos="fade-up">
           <img src={phone} alt="phone" />
           <h6>Bizi arayın</h6>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam quam iste, voluptatum perferendis similique repudiandae culpa architecto excepturi doloremque iure.</p>
+          <p>(0252) 225 34 34</p>
         </div>
       </div>
     </section>
