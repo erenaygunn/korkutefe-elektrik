9bd6e03bcb26bd98d496eee601c56346c29f3ad2
erenaygunn <eaygun178@gmail.com>
2023-10-15 20:58:20 +0300

    hizmetler p

--- src/components/Hizmetler/Hizmetler.jsx
+++ src/components/Hizmetler/Hizmetler.jsx
@@ -21,6 +21,7 @@ const Hizmetler = () => {
             <Card icon={fix} title='Arıza Giderme'>Arıza giderme hizmetimizle elektrik sorunlarınızı hızlı ve güvenilir bir şekilde çözüyoruz. Deneyimli ekibimiz, elektrik kesintilerini teşhis eder, onarımları hızla yapar ve kesintisiz enerji sağlar. Elektrik sistemlerinizde güvenilirlik ve sürekli işlerlik sağlamak için buradayız.</Card>
           </div>
         </div>
+        <p>Sunduğumuz diğer hizmetler dahil olmak üzere bilgi almak için <a href='#'> bize ulaşın.</a></p>
     </section>
   )
 }
