4e21a506592319267242675857c65e089425d206
erenaygunn <eaygun178@gmail.com>
2023-10-16 00:16:12 +0300

    scroll to top button

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Hero from './components/Hero/Hero'
 import Hizmetler from './components/Hizmetler/Hizmetler'
 import Projeler from './components/Projeler/Projeler'
 import Iletisim from './components/iletisim/iletisim'
+import ScrollToTop from './components/ScrollToTop/ScrollToTop'
 
 
 function App() {
 
   return (
     <div>
+      <ScrollToTop></ScrollToTop>
       <Hero></Hero>
       <Hizmetler></Hizmetler>
       <Projeler></Projeler>
---
+++ src/components/ScrollToTop/ScrollToTop.jsx
@@ -0,0 +1,39 @@
+import "./scrolltotop.css"
+import React, { useState, useEffect } from 'react';
+import { FaArrowCircleUp } from 'react-icons/fa';
+import uparrow from "../../images/uparrow.png"
+
+const ScrollToTop = () => {
+
+    const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.scrollY > 100) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility);
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility);
+    };
+  }, []);
+
+  return (
+    <div className={`scroll-to-top ${isVisible ? 'visible' : ''}`}>
+      <button onClick={scrollToTop}><img src={uparrow} alt="up-arrow" /></button>
+    </div>
+  )
+}
+
+export default ScrollToTop
