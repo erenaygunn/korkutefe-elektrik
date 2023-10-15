424abbb691b777e961eeea0d83bfe6e4a724b44b
erenaygunn <eaygun178@gmail.com>
2023-10-16 00:18:20 +0300

    hero button fix

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
         <div className="container">
           <h1>Korkut Efe Elektriğe Hoş geldiniz</h1>
           <h4>Muğla ve civarındaki bütün elektrik işlerinizde <br /> bize güvenebilirsiniz.</h4>
-          <button className='action'><a href="#hizmetler">DEVAM ET</a></button>
+          <a href="#hizmetler"><button className="action">DEVAM ET</button></a>
         </div>
     </section>
   )
